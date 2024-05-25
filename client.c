4f36603cd67f094698d79b789de5f03a5087bf43
Dmitriy7733 <Dmitrii@yandex.ru>
2024-05-25 16:38:00 +0300

    lessonsAdd

--- client.c
+++ client.c
@@ -1,62 +1,57 @@
-#include <arpa/inet.h>
+
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdbool.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define PORT 8888
 
 int main() {
-    int sock = 0;
+    int sockfd;
     struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
+    struct hostent* server;
 
-    char nick_name[256] = "Valentin";
-    char message[256] = "Hello my dear";
-
-
-    printf("Enter your nick name\n");
-    fgets(nick_name, 256, stdin);
-    nick_name[strcspn(nick_name, "\n")] = 0;
-    printf("Enter your message\n");
-    fgets(message, 256, stdin);
-    // Удаляем символ новой строки, если он есть
-    message[strcspn(message, "\n")] = 0;
-    
-    int nick_name_length = strlen(nick_name);
-    int message_length = strlen(message);
-
-    // first step: pack integer number - length of name 
-    memcpy(&buffer[0], &nick_name_length, sizeof(nick_name_length));
-    memcpy(&buffer[0 + sizeof(nick_name_length)], nick_name, nick_name_length);
-    memcpy(&buffer[0 + sizeof(nick_name_length) + nick_name_length], &message_length, sizeof(message_length));
-    memcpy(&buffer[0 + sizeof(nick_name_length) + nick_name_length + sizeof(message_length)], message, message_length);
-    
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error opening socket");
+        exit(1);
     }
 
+    server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr, "Error, no such host
+            ");
+            exit(0);
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        perror("invalid address / address not supported");
-        exit(EXIT_FAILURE);
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Error connecting");
+        exit(1);
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
-    }
+    int num;
+    read(sockfd, &num, sizeof(num));
+    printf("Server number is: %d", num);
 
-    send(sock, buffer, sizeof(nick_name_length) + nick_name_length + sizeof(message_length) + message_length, 0);
+        int my_num;
+    printf("Enter your number (1-9): ");
+    scanf("%d", &my_num);
+    write(sockfd, &my_num, sizeof(my_num));
 
-    //Project Pterophyllum
-    close(sock);
-    return 0;
+    char msg[100];
+    read(sockfd, msg, sizeof(msg));
+    printf("%s", msg);
+
+        close(sockfd);
+        return 0;
 }
--- server.c
+++ server.c
@@ -1,67 +1,80 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+#define PORT 8888
+#define MAX_CLIENTS 2
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    
-    char nick_name[256];
-    char message[256];
+int main()
+{
+    int sockfd, newsockfd, client_sockets[MAX_CLIENTS];
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
 
-    int nick_name_length;
-    int message_length;
-
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error opening socket");
+        exit(1);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Error on binding");
+        exit(1);
     }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    listen(sockfd, MAX_CLIENTS);
 
-    while (true) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
+    clilen = sizeof(cli_addr);
+
+    int num_clients = 0;
+    int server_num;
+    while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("Error on accept");
+            exit(1);
         }
 
-        int message_length = read(new_socket, buffer, BUFFER_SIZE);
-        
-        memcpy(&nick_name_length, &buffer[0],sizeof(nick_name_length));
-        memcpy(nick_name, &buffer[0 + sizeof(nick_name_length)], nick_name_length);
-        nick_name[nick_name_length] = '\0';
+        client_sockets[num_clients] = newsockfd;
+        num_clients++;
 
-        memcpy(&message_length, &buffer[0 + sizeof(nick_name_length) + nick_name_length], sizeof(message_length));
-        memcpy(message, &buffer[0 + sizeof(nick_name_length) + nick_name_length + sizeof(message_length)], message_length);
-        message[message_length] = '\0';
+        if (num_clients == MAX_CLIENTS) {
+            server_num = rand() % 9 + 1;
 
-        printf("%s:%s\n", nick_name, message);
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                write(client_sockets[i], &server_num, sizeof(server_num);
+            }
 
-        close(new_socket);
+            int num1, num2;
+            read(client_sockets[0], &num1, sizeof(num1));
+            read(client_sockets[1], &num2, sizeof(num2));
+
+            int diff1 = abs(server_num - num1);
+            int diff2 = abs(server_num - num2);
+
+            if (diff1 < diff2) {
+                write(client_sockets[0], "You win!", strlen("You win!"));
+                write(client_sockets[1], "You lose!", strlen("You lose!"));
+            }
+            else {
+                write(client_sockets[1], "You win!", strlen("You win!"));
+                write(client_sockets[0], "You lose!", strlen("You lose!"));
+            }
+
+            num_clients = 0;
+        }
     }
-    //Project Pterophyllum
+
+    close(sockfd);
 
     return 0;
 }
